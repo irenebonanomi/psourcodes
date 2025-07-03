92071642a556e01c4e50aa54422decd155e7de79
irenebonanomi <121675900+irenebonanomi@users.noreply.github.com>
2025-07-03 10:48:59 -0600

    Add files via upload

---
+++ findPM.py
@@ -0,0 +1,9 @@
+import pyvisa, textwrap
+rm = pyvisa.ResourceManager('@py')
+for addr in rm.list_resources():
+    try:
+        idn = rm.open_resource(addr, read_termination='\n').query('*IDN?')
+        print(f"{addr:40} → {idn.strip()}")
+    except pyvisa.VisaIOError:
+        pass
+
